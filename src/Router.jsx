413c39094f91ae765214dd0aaa0c6812e00a89b6
mhadev0703 <minchulha0703@gmail.com>
2024-04-03 23:53:54 -0400

    Router basic setup

--- src/Router.jsx
+++ src/Router.jsx
@@ -9,15 +9,21 @@ import KitchenProfilePage from './pages/KitchenProfile';
 import CheckoutPage from './pages/Checkout';
 import PaymentPage from './pages/Payment';
 import OrderConfirmationPage from './pages/OrderConfirmation';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<MainPage />} />
-                
+                <Route path="/" element={<MainPage />} />                
+                <Route path="/kitchen-page" element={<KitchenPage />} />                
+                <Route path="/create-account" element={<AccountCreationPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/reset-password" element={<PasswordResetPage />} />
+                <Route path="/profile" element={<UserProfilePage />} />
+                <Route path="/kitchen-profile" element={<KitchenProfilePage />} />
+                <Route path="/checkout" element={<CheckoutPage />} />
+                <Route path="/payment" element={<PaymentPage />} />
+                <Route path="/order-confirmation" element={<OrderConfirmationPage />} />
             </Routes>
         </BrowserRouter>
     );
